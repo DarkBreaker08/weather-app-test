e35cb79074aeebc2851847fac5b91716869de615
Gregory <grzesb1508@wp.pl>
2025-03-18 19:32:15 +0100

    Added WeatherStatistics component to WeatherParameters component

--- src/Components/WeatherParameters/WeatherParameters.tsx
+++ src/Components/WeatherParameters/WeatherParameters.tsx
@@ -1,10 +1,12 @@
 import { CitySelect } from "../CitySelect/CitySelect";
+import { WeatherStatistics } from "../WeatherStatistics/WeatherStatistics";
 import styles from "./styles.module.scss";
 
 export const WeatherParameters = () => {
   return (
     <div className={styles.weatherParameters}>
       <CitySelect />
+      <WeatherStatistics />
     </div>
   );
 };
