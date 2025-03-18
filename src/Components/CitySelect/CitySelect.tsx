02641d6ff454745435ba1795a77f0916833f9a3d
Gregory <grzesb1508@wp.pl>
2025-03-18 19:07:14 +0100

    Created CitySelect component and its styles

---
+++ src/Components/CitySelect/CitySelect.tsx
@@ -0,0 +1,27 @@
+import styles from "./styles.module.scss";
+
+export const CitySelect = () => {
+  return (
+    <select name="cities" id="citySelect" className={styles.citySelect}>
+      <option value="Warsaw">Warsaw, Poland</option>
+      <option value="London">London, Great Britain</option>
+      <option value="Moscow">Moscow, Russia</option>
+      <option value="Paris">Paris, France</option>
+      <option value="Madrid">Madrid, Spain</option>
+      <option value="Barcelona">Barcelona, Spain</option>
+      <option value="Rome">Rome, Italy</option>
+      <option value="Berlin">Berlin, Germany</option>
+      <option value="Kyiv">Kyiv, Ukraine</option>
+      <option value="Baku">Baku, Azerbaijan</option>
+      <option value="Vienna">Vienna, Austria</option>
+      <option value="Minsk">Minsk, Belarus</option>
+      <option value="Hamburg">Hamburg, Germany</option>
+      <option value="Bucharest">Bucharest, Romania</option>
+      <option value="Budapest">Budapest, Hungary</option>
+      <option value="Belgrade">Belgrade, Serbia</option>
+      <option value="Munich">Munich, Germany</option>
+      <option value="Kharkiv">Kharkiv, Ukraine</option>
+      <option value="Prague">Prague, Czech Republic</option>
+    </select>
+  );
+};
