2eafa819817cca7a9e141a087e23ff28d7b87685
Gregory <grzesb1508@wp.pl>
2025-03-18 19:49:01 +0100

    Added main components to Weather component

--- src/Components/Weather/Weather.tsx
+++ src/Components/Weather/Weather.tsx
@@ -1,5 +1,12 @@
+import { CurrentWeather } from "../CurrentWeather/CurrentWeather";
+import { WeatherParameters } from "../WeatherParameters/WeatherParameters";
 import styles from "./styles.module.scss";
 
 export const Weather = () => {
-  return <header className={styles.weather}></header>;
+  return (
+    <header className={styles.weather}>
+      <CurrentWeather />
+      <WeatherParameters />
+    </header>
+  );
 };
