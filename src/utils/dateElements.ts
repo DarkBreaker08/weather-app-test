aaa98a8adee7b1d06b897bde7072a0de72d23975
Gregory <grzesb1508@wp.pl>
2025-03-19 14:28:21 +0100

    Created date vars

---
+++ src/utils/dateElements.ts
@@ -0,0 +1,26 @@
+export const apiKey = "db83b7ac88a44bcc83d175016252101";
+
+export const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const weekDays = [
+  "Monday",
+  "Tuesday",
+  "Wendsday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
